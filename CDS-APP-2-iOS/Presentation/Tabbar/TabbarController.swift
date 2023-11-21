05f59e8e8171fe1e8c9f0233686b9086b8743899
seoyeon <102604192+choiseoo@users.noreply.github.com>
2023-11-22 00:24:51 +0900

    [Feat] #4 TabbarController 구현

---
+++ CDS-APP-2-iOS/Presentation/Tabbar/TabbarController.swift
@@ -0,0 +1,69 @@
+//
+//  TabbarController.swift
+//  CDS-APP-2-iOS
+//
+//  Created by 최서연 on 11/22/23.
+//
+
+import UIKit
+
+final class TabbarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setUI()
+        self.addTabBarController()
+    }
+    
+    private func setUI() {
+        self.view.backgroundColor = .white
+        tabBar.backgroundColor = .background
+    }
+    
+    private func addTabBarController() {
+        var tabNavigationControllers = [UINavigationController]()
+        
+        for item in TabbarItem.allCases {
+            let tabNavController = createTabNavigationController(
+                title: item.description,
+                image: item.icon,
+                selectedImage: item.selectedIcon
+            )
+            tabNavigationControllers.append(tabNavController)
+        }
+        
+        setViewControllers(tabNavigationControllers, animated: false)
+    }
+    
+    private func createTabNavigationController(title: String, image: UIImage, selectedImage: UIImage) -> UINavigationController {
+        let tabNavigationController = UINavigationController()
+        
+        let tabbarItem = UITabBarItem(
+            title: title,
+            image: image.withRenderingMode(.alwaysOriginal),
+            selectedImage: selectedImage.withRenderingMode(.alwaysOriginal)
+        )
+        
+        // title이 선택되지 않았을 때 폰트, 색상 설정
+        let normalAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.enDisplayMedium(ofSize: 8.adjusted),
+            .foregroundColor: UIColor.darkGray
+        ]
+        
+        // title이 선택되었을 때 폰트, 색상 설정
+        let selectedAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.enDisplayBold(ofSize: 8.adjusted),
+            .foregroundColor: UIColor.black
+        ]
+        
+        tabbarItem.setTitleTextAttributes(normalAttributes, for: .normal)
+        tabbarItem.setTitleTextAttributes(selectedAttributes, for: .selected)
+        
+        tabNavigationController.tabBarItem = tabbarItem
+        tabNavigationController.isNavigationBarHidden = true
+        
+        return tabNavigationController
+    }
+}
+
