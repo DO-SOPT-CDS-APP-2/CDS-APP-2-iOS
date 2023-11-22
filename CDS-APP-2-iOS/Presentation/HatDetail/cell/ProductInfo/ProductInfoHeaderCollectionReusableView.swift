d8722b99aabbfe0a545c68cc852b4f32f63de014
mcrkgus <mcrkgus@naver.com>
2023-11-22 16:57:36 +0900

    [Add] ProductInfo Cell Header 추가

---
+++ CDS-APP-2-iOS/Presentation/HatDetail/cell/ProductInfo/ProductInfoHeaderCollectionReusableView.swift
@@ -0,0 +1,105 @@
+//
+//  ProductInfoHeaderCollectionReusableView.swift
+//  CDS-APP-2-iOS
+//
+//  Created by Gahyun Kim on 2023/11/22.
+//
+
+import UIKit
+
+class ProductInfoHeaderCollectionReusableView: UICollectionReusableView {
+    
+    static let identifier: String = "ProductInfoHeaderCollectionReusableView"
+    
+    private let productInfoLabel = UILabel()
+    private let sizeLabel = UILabel()
+    private let recommendLabel = UILabel()
+    private let reviewLabel = UILabel()
+    private let inquireLabel = UILabel()
+    private let selectlineView = UIView()
+    
+    private let selectStackView = UIStackView()
+    private let headerStackView = UIStackView()
+    
+    
+    // MARK: - Set UI
+    
+    private func setUI() {
+        productInfoLabel.do {
+            $0.textColor = .black
+            $0.font = .krBold(ofSize: 14)
+        }   //상품정보
+        
+        selectlineView.do {
+            $0.backgroundColor = .black
+        }   //selectline w:67길이
+        
+        selectStackView.do {
+            $0.axis = .vertical
+            $0.spacing = 3
+        }
+        
+        sizeLabel.do {
+            $0.textColor = .lightGray
+            $0.font = .krBold(ofSize: 14)
+        }   //사이즈
+        
+        recommendLabel.do {
+            $0.textColor = .lightGray
+            $0.font = .krBold(ofSize: 14)
+        }   //추천
+        
+        reviewLabel.do {
+            $0.textColor = .lightGray
+            $0.font = .krBold(ofSize: 14)
+        }   //리뷰
+        
+        inquireLabel.do {
+            $0.textColor = .lightGray
+            $0.font = .krBold(ofSize: 14)
+        }   //문의
+        
+   
+        headerStackView.do {
+            $0.axis = .horizontal
+            $0.distribution = .equalSpacing
+        }
+    }
+    
+    
+    // MARK: - Set Hierachy
+    
+    private func setHierachy() {
+        selectStackView.addArrangedSubview(productInfoLabel)
+        selectStackView.addArrangedSubview(selectlineView)
+        headerStackView.addArrangedSubview(selectStackView)
+        headerStackView.addArrangedSubviews(sizeLabel, recommendLabel, reviewLabel, inquireLabel)
+
+        addSubview(headerStackView)
+    }
+    
+    
+    // MARK: - Set Layout
+    
+    private func setLayout() {
+        selectlineView.snp.makeConstraints {
+            $0.width.equalTo(67)
+            $0.height.equalTo(1)
+        }
+        
+        headerStackView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(20)
+            $0.leading.equalToSuperview().inset(30)
+            $0.trailing.equalToSuperview().inset(30)
+        }
+    }
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
