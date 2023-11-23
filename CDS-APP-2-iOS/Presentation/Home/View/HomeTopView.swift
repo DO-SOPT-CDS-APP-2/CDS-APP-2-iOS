2c246f62d5616f9cc23c2d522bb354d14287061f
binisnil <dbsqlssss0704@gmail.com>
2023-11-24 01:04:57 +0900

    [Chore] #12 - adjusted 추가

--- CDS-APP-2-iOS/Presentation/Home/View/HomeTopView.swift
+++ CDS-APP-2-iOS/Presentation/Home/View/HomeTopView.swift
@@ -67,17 +67,17 @@ final class HomeTopView: UIView {
         
         topButtonStackView.do {
             $0.axis = .horizontal
-            $0.spacing = 15
+            $0.spacing = 15.adjusted
         }
         
         chipStackView.do {
             $0.axis = .horizontal
-            $0.spacing = 8
+            $0.spacing = 8.adjusted
         }
         
         // 맨 처음 ChipView만 label font가 다르기 때문에, 첫번째 요소만 가져와 폰트 변경해주는 코드 작성
         if let firstSubview = chipStackView.arrangedSubviews.first as? ChipView {
-            firstSubview.tagLabel.font = .krSemiBold(ofSize: 12)
+            firstSubview.tagLabel.font = .krSemiBold(ofSize: 12.adjusted)
             firstSubview.tagLabel.textColor = .white
             firstSubview.layer.borderColor = UIColor.white.cgColor
         }
@@ -102,31 +102,31 @@ final class HomeTopView: UIView {
         }
         logoImageView.snp.makeConstraints {
             $0.top.equalToSuperview().inset(64.adjusted)
-            $0.leading.equalToSuperview().inset(18)
+            $0.leading.equalToSuperview().inset(18.adjusted)
         }
         
         searchButton.snp.makeConstraints {
-            $0.height.width.equalTo(24)
+            $0.height.width.equalTo(24.adjusted)
         }
         
         alertButton.snp.makeConstraints {
-            $0.height.width.equalTo(24)
+            $0.height.width.equalTo(24.adjusted)
         }
         
         cartButton.snp.makeConstraints {
-            $0.height.width.equalTo(24)
+            $0.height.width.equalTo(24.adjusted)
         }
         
         topButtonStackView.snp.makeConstraints {
             $0.centerY.equalTo(logoImageView)
-            $0.trailing.equalToSuperview().inset(17)
-            $0.height.equalTo(24)
+            $0.trailing.equalToSuperview().inset(17.adjusted)
+            $0.height.equalTo(24.adjusted)
         }
         
         chipStackView.snp.makeConstraints {
-            $0.top.equalTo(logoImageView.snp.bottom).offset(15)
-            $0.leading.equalToSuperview().inset(18)
-            $0.height.equalTo(30)
+            $0.top.equalTo(logoImageView.snp.bottom).offset(15.adjusted)
+            $0.leading.equalToSuperview().inset(18.adjusted)
+            $0.height.equalTo(30.adjusted)
         }
     }
 }
