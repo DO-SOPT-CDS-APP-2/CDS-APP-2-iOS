64d25834bfe8da36b4eb4601aee08a1831f7917d
SeoYeon Choi <102604192+choiseoo@users.noreply.github.com>
2023-11-23 15:00:40 +0900

    [Feat] #13 헤더 가로 컬렉션뷰 레이아웃 구성

--- CDS-APP-2-iOS/Presentation/HatCategory/ViewController/HatCategoryViewController.swift
+++ CDS-APP-2-iOS/Presentation/HatCategory/ViewController/HatCategoryViewController.swift
@@ -8,14 +8,47 @@
 import UIKit
 
 import SnapKit
+import Then
 
 final class HatCategoryViewController: UIViewController {
-
+    
+    //MARK: set Property
+    
+    private let headerCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         
         self.setNavigationBar()
+        self.setUI()
+        self.setHierachy()
+        self.setLayout()
+    }
+    
+    //MARK: set UI
+    
+    private func setUI() {
+        headerCollectionView.do {
+            $0.backgroundColor = .clear
+            $0.showsHorizontalScrollIndicator = false
+        }
+    }
+    
+    //MARK: set Hierachy
+    
+    private func setHierachy() {
+        self.view.addSubview(headerCollectionView)
+    }
+    
+    //MARK: set Layout
+    
+    private func setLayout() {
+        headerCollectionView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(88.adjusted) // 이렇게밖에 안된다고 ..??? 수정해봐
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(43.adjusted)
+        }
     }
     
     //MARK: set NavigationBar
