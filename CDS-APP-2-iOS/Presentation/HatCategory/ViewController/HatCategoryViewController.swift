6969174e58faf2f6322965f677106253ab865746
SeoYeon Choi <102604192+choiseoo@users.noreply.github.com>
2023-11-25 15:34:54 +0900

    [Del] #22 - 불필요한 코드 삭제

--- CDS-APP-2-iOS/Presentation/HatCategory/ViewController/HatCategoryViewController.swift
+++ CDS-APP-2-iOS/Presentation/HatCategory/ViewController/HatCategoryViewController.swift
@@ -147,28 +147,3 @@ extension HatCategoryViewController: UICollectionViewDataSource {
         return item
     }
 }
-
-extension HatCategoryViewController {
-    private func createLayout() -> UICollectionViewLayout {
-        let configuration = UICollectionViewCompositionalLayoutConfiguration()
-        let layout = createCompositionalLayout()
-        layout.configuration = configuration
-        return layout
-    }
-    
-    private func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
-        return UICollectionViewCompositionalLayout { (sectionIndex, NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
-                                                  heightDimension: .fractionalHeight(1))
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-            
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
-                                                   heightDimension: .absolute(140))
-            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-            
-            let section = NSCollectionLayoutSection(group: group)
-            return section
-        }
-        
-    }
-}
