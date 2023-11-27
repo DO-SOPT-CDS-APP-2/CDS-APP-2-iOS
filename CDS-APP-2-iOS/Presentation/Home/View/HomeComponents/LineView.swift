beb80af6cf0842a681a2d9a2d57be18e2e98c65c
binisnil <dbsqlssss0704@gmail.com>
2023-11-27 21:10:37 +0900

    [Chore] #29 - 컴포넌트로 사용되는 요소 따로 폴더링

---
+++ CDS-APP-2-iOS/Presentation/Home/View/HomeComponents/LineView.swift
@@ -0,0 +1,43 @@
+//
+//  LineView.swift
+//  CDS-APP-2-iOS
+//
+//  Created by 박윤빈 on 2023/11/27.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+// MARK: - Home의 최상단 View
+
+final class LineView: UIView {
+
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Functions
+    
+    private func setUI() {
+        self.backgroundColor = .background
+    }
+        
+    private func setLayout() {
+        self.snp.makeConstraints {
+            $0.height.equalTo(0.96)
+        }
+    }
+}
+
