0d3dab856e66c1fd1a13b444bbfe60a38061feb0
SeoYeon Choi <102604192+choiseoo@users.noreply.github.com>
2023-11-28 17:32:26 +0900

    [Feat] #36 FilterCategory 컬렉션뷰 Cell 구현

---
+++ CDS-APP-2-iOS/Presentation/HatCategory/Cell/FilterCategoryCollectionViewCell.swift
@@ -0,0 +1,76 @@
+//
+//  FilterCategoryCollectionViewCell.swift
+//  CDS-APP-2-iOS
+//
+//  Created by 최서연 on 11/28/23.
+//
+
+import UIKit
+
+final class FilterCategoryCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: - set Properties
+    
+    private let label = UILabel()
+    private let downDetailImageView = UIImageView()
+
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setUI()
+        self.setHierachy()
+        self.setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - set UI
+    
+    private func setUI() {
+        
+        self.do {
+            $0.layer.addBorder([.all], color: .border, width: 1)
+            $0.layer.cornerRadius = 18.adjusted
+        }
+        
+        label.do {
+            $0.font = UIFont.krSemiBold(ofSize: 12.adjusted)
+            $0.textColor = .lightGray
+        }
+        
+        downDetailImageView.do {
+            $0.image = ImageLiterals.icon.icDetailDownGraySmall
+        }
+    }
+    
+    //MARK: - set Hierachy
+    
+    private func setHierachy() {
+        self.addSubviews(label, downDetailImageView)
+    }
+    
+    //MARK: - set Layout
+    
+    private func setLayout() {
+        label.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(9.adjusted)
+            $0.leading.equalToSuperview().inset(14.adjusted)
+        }
+        
+        downDetailImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(9.adjusted)
+            $0.leading.equalTo(label.snp.trailing).offset(5.adjusted)
+        }
+    }
+    
+    //MARK: - Methods
+    
+    private func bindData(filterCategory: String) {
+        label.text = filterCategory
+    }
+    
+}
