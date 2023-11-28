da5b86d553ca3a1d462cdd4729b23f61802c34e2
mcrkgus <mcrkgus@naver.com>
2023-11-28 19:13:59 +0900

    [Feat] #33 - SecondRecommendCollectionView Cell - 관련 이벤트 구현

---
+++ CDS-APP-2-iOS/Presentation/HatDetail/cell/RecommendInfo/SecondRecommendCollectionViewCell.swift
@@ -0,0 +1,74 @@
+//
+//  SecondRecommendCollectionViewCell.swift
+//  CDS-APP-2-iOS
+//
+//  Created by Gahyun Kim on 2023/11/28.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class SecondRecommendCollectionViewCell: UICollectionViewCell {
+    
+    private let eventImage = UIImageView()
+    private let eventTitleLabel = UILabel()
+    private let eventDurationLabel = UILabel()
+    private let eventStackView = UIStackView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierachy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Set UI
+    
+    private func setUI() {
+        eventTitleLabel.do {
+            $0.font = .krBold(ofSize: 12.adjusted)
+            $0.textColor = .black
+        }
+        
+        eventDurationLabel.do {
+            $0.font = .enDisplayMedium(ofSize: 10.adjusted)
+            $0.textColor = .mediumGray
+        }
+        
+        eventStackView.do {
+            $0.axis = .vertical
+            $0.spacing = 10
+        }
+    }
+    
+    // MARK: - Set Hierachy
+    
+    private func setHierachy() {
+        eventStackView.addArrangedSubviews(eventImage, eventTitleLabel, eventDurationLabel)
+        contentView.addSubview(eventStackView)
+    }
+    
+    // MARK: - Set Layout
+    
+    private func setLayout() {
+        eventStackView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.width.equalTo(217.adjusted)
+            $0.height.equalTo(201.adjusted)
+        }
+    }
+    
+    
+    func bindData(item: EventItem) {
+        eventImage.image = item.image
+        eventTitleLabel.text = item.eventTitle
+        eventDurationLabel.text = item.eventDuration
+    }
+}
