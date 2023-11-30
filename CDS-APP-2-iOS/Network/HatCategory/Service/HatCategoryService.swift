77a061cbfb3b1e3cc1fb131992b71204d07c3910
SeoYeon Choi <102604192+choiseoo@users.noreply.github.com>
2023-11-30 16:04:39 +0900

    [Feat] #40 HatCategoryService 함수 추가

---
+++ CDS-APP-2-iOS/Network/HatCategory/Service/HatCategoryService.swift
@@ -0,0 +1,75 @@
+//
+//  HatCategoryService.swift
+//  CDS-APP-2-iOS
+//
+//  Created by 최서연 on 11/30/23.
+//
+
+import Foundation
+
+final class HatCategoryService {
+    // 싱글톤 객체 생성
+    static let shared = HatCategoryService()
+    private init() {}
+    
+    func makeRequestURL(categoryId: Int) -> URLRequest {
+        let baseURL = Bundle.main.object(forInfoDictionaryKey: Config.Keys.Plist.baseURL) as? String ?? ""
+        
+        let urlString = "\(baseURL)/api/v1/category/\(categoryId)"
+        
+        guard let url = URL(string: urlString) else {
+            fatalError("Failed to create URL")
+        }
+        
+        // URLRequest 생성
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        // 헤더 추가
+        let header = ["Content-Type": "application/json"]
+        header.forEach {
+            request.addValue($0.value, forHTTPHeaderField: $0.key)
+        }
+        
+        return request
+    }
+    
+    func getHatCategoryWithAPI(categoryId: Int) async throws -> CityWeatherResponseDTO? {
+        do {
+            let request = self.makeRequestURL(categoryId: categoryId)
+            let (data, response) = try await URLSession.shared.data(for: request)
+            guard let httpResponse = response as? HTTPURLResponse else {
+                throw NetworkError.responseError
+            }
+            if httpResponse.statusCode == 200 {
+                // 성공했을 때
+                return parseHatCategoryData(data: data)
+            }
+            else if httpResponse.statusCode == 404 {
+                // 카테고리 아이디가 잘못된 경우, 해당 상품이 없는 경우
+                throw NetworkError.notFoundError
+            }
+            else if httpResponse.statusCode == 500 {
+                // 서버 내부 오류
+                throw NetworkError.internalServerError
+            }
+            else {
+                // 이외의 오류
+                throw NetworkError.unknownError
+            }
+        } catch {
+            throw error
+        }
+    }
+    
+    private func parseHatCategoryData(data: Data) -> CityWeatherResponseDTO? {
+        do {
+            let jsonDecoder = JSONDecoder()
+            let result = try jsonDecoder.decode(CityWeatherResponseDTO.self, from: data)
+            return result
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+}
