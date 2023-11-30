f247ef45d8d663acd32576b4f63736efa58c53af
binisnil <dbsqlssss0704@gmail.com>
2023-11-30 16:59:14 +0900

    [Chore] #57 - API 통신 코드 작성

---
+++ CDS-APP-2-iOS/Network/Home/Sevice/HomePromotionService.swift
@@ -0,0 +1,54 @@
+//
+//  HomePromotionService.swift
+//  CDS-APP-2-iOS
+//
+//  Created by 박윤빈 on 2023/11/30.
+//
+
+import UIKit
+
+class HomePromotionService {
+    static let shared = HomePromotionService()
+    private init() {}
+    
+    func makeRequest() -> URLRequest {
+        let baseURL = Bundle.main.object(forInfoDictionaryKey: Config.Keys.Plist.baseURL) as? String ?? ""
+        let url = URL(string: baseURL + StringLiterals.Home.thirdSection.categoryURL)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let header = ["Content-Type": "application/json"]
+        header.forEach {
+            request.addValue($0.value, forHTTPHeaderField: $0.key)
+        }
+        return request
+    }
+    
+    func getPromotionData() async throws -> PromotionDataModel? {
+        do {
+            let request = self.makeRequest()
+            let (data, response) = try await URLSession.shared.data(for: request)
+            guard let httpResponse = response as? HTTPURLResponse else {
+                throw NetworkError.responseError
+            }
+            return parsePromotionData(data: data)
+        } catch {
+            throw error
+        }
+    }
+    
+    func parsePromotionData(data: Data) -> PromotionDataModel? {
+        do {
+            let jsonDecoder = JSONDecoder()
+            let result = try jsonDecoder.decode(PromotionDataModel.self, from: data)
+            return result
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    private func configureHTTPError(errorCode: Int) -> Error {
+        return NetworkError(rawValue: errorCode)
+        ?? NetworkError.unknownError
+    }
+}
